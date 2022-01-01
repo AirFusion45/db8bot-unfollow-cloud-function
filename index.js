2d3a7afe44dc72b785fa41eeccec2de5f4167d56
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-01-01 10:23:27 -0800

    prod: trying to debug a 502 from CF

--- index.js
+++ index.js
@@ -7,20 +7,23 @@ exports.unfollow = (req, resApp) => {
         username: req.body.tabUser,
         password: req.body.tabPass
     }
+    console.log('not in requests yet')
     superagent
         .post('https://debateapis.wm.r.appspot.com/login')
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .send(JSON.parse(JSON.stringify(payload)))
         .end((err, res) => {
             if (err) console.error(err)
+            console.log(`req1 done: ${res.body}`)
             superagent
                 .get(unfollowLink)
                 .set('Cookie', res.body.token)
                 .redirects(1)
                 .end((err, res) => {
-                    if (err) console.error(err)
+                    if (err) console.error(`Err @ 2nd req: ${err}`)
                     if (res.text.includes('Stop Your Live Updates')) {
                         resApp.status(200).send('Unfollowed')
+                        console.log('Unfollowed')
                     }
                 })
         })
