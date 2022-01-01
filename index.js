416dc0eaf422d3d19dfbaf5747c65344a0f3f3ca
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-01-01 11:12:01 -0800

    prod: it was an io issue - very stupid one

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ exports.unfollow = (req, resApp) => {
 
     var payload = {
         apiauth: req.body.tabapiauth,
-        username: req.body.tabUser,
-        password: req.body.tabPass
+        username: req.body.username,
+        password: req.body.password
     }
-    console.log('not in requests yet')
-    console.log(unfollowLink)
-    console.log(payload)
     superagent
         .post('https://debateapis.wm.r.appspot.com/login')
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .send(JSON.parse(JSON.stringify(payload)))
         .end((err, res) => {
             if (err) console.error(`Err @ 1st req: ${err}`)
-            console.log(`req1 done: ${JSON.stringify(res.body)}`)
             superagent
                 .get(unfollowLink)
                 .set('Cookie', res.body.token)
@@ -25,7 +21,6 @@ exports.unfollow = (req, resApp) => {
                     if (err) console.error(`Err @ 2nd req: ${err}`)
                     if (res.text.includes('Stop Your Live Updates')) {
                         resApp.status(200).send('Unfollowed')
-                        console.log('Unfollowed')
                     }
                 })
         })
